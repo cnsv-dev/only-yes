1290e5eca043fc3ad18d4cd597ac964392988c8f
Paul T. <45326403+katistix@users.noreply.github.com>
2024-02-05 13:11:15 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,67 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [posLeft, setPosLeft] = useState(0);
+  const [posTop, setPosTop] = useState(0);
+  const [yesScale, setYesScale] = useState(1);
+  const [text, setText] = useState("Vrei sa iesim la un date?");
+  const [ok, setOk] = useState(false);
+
+  function noClicked(){
+    let newLeft = Math.random() * 100;
+    setPosLeft(newLeft);
+
+    let newTop = Math.random() * 300;
+    setPosTop(newTop);
+
+    setYesScale(yesScale + 0.5);
+
+    if(ok===true){
+      setText("De ce te-ai razgandit?");
+    }
+  }
+
+  function yesClicked(){
+    if(ok==true){
+      setText("Asa ramane ;) ❤️")
+    }
+    else{
+      setText("Ai ales corect <3")
+    }
+
+    setYesScale(1);
+    setOk(true);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1 className="text">{text}</h1>
+
+      <div className='butoane'>
+        <button className='buton1'
+        onClick={yesClicked}
+        style={{
+          transform: "scale("+ yesScale.toString() + ")"
+        }}>Da</button>
+
+        <button className='buton2' style={{
+          position:'absolute',
+          top: posTop+40,
+          left: posLeft+150,
+        }}
+        onMouseEnter={noClicked}
+        >Nu</button>
+      </div>
     </div>
   );
 }
+// function Crestere(){
+//   <div className='Crestere'>
+
+//   </div>
+// }
 
 export default App;
